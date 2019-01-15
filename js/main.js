494ba9739791adb80c4ed7649ca4230f485d62ad
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-01-15 16:44:05 -0500

    refactor app.js
    refactor app.js and index.html code

--- js/main.js
+++ js/main.js
@@ -1,35 +1,59 @@
-// DEFINE VARIABLES AND SETTINGS
+/////////////////////////////////////////////////////////////
+//  BodyCircuits/z
+//  Simple random bodyweight workout generator
+//
+//  Author: Jeremy Willhelm
+/////////////////////////////////////////////////////////////
+
+// EXERCISES & REPS
 var exercises = ['squats', 'crunches', 'pushups', 'pikes'];
 var reps = [2,3,4,5,6,7,8,9,10,10,10,10,15];
+var wildcardExercise1 = {
+    exercise: "do all",
+    reps: 5
+};
+var wildcardExercise2 ={
+    exercise: "do all",
+    reps: 8
+};
+
+// APP VARIABLES
 var workoutList = []; // Array contains exercise + reps per exercise
 var completedExercises = [] // List of exercises completed from workout list
-var currentExerciseIndex = 0;
-var breakCounter = 0;
+var currentExerciseIndex = 0; // Workout list initializer
+var workoutComplete = false;
+
+// APP SETTINGS
+var settings = {
+    restTimer: 2, // TIMER FOR REST PERIODS - Default: 2 mins
+    totalCounter: 0, // COUNTER FOR TOTAL EXERCISES COMPLETED
+    breakCounter: 0 // COUNTER TO KEEP TRACK WHEN START REST PERIODS
+}
 
 
 // PAGE ELEMENTS & NODES
-var totalCounter = 0;
-var totalCounterEle = document.getElementById('total_exercise_count');
-var exerciseTitleEle = document.getElementById('exercise_title');
-var exerciseRepEle = document.getElementById('exercise_count');
+var nodes = {
+    totalCounter: document.getElementById('total_exercise_count'),
+    exercise: document.getElementById('exercise_title'),
+    reps: document.getElementById('exercise_count')
+}
 
-// ADD EXERCISE TO LIST
+/////////////////////////////////////////////////////////////
+// ADD EXERCISE TO EXERCISE LIST
+/////////////////////////////////////////////////////////////
 function addExercise(name){
-    
-    // check that name is not empty string
-    if(name === "" || name === undefined){
-        return;
-    }
     
     exercises.push(name);
 }
 
+/////////////////////////////////////////////////////////////
 // CREATE WORKOUT
+/////////////////////////////////////////////////////////////
 function createWorkout(){
    
   var i, j;
 
-  // FILL WORKOUT LIST WITH EXERCISE AND REP SCHEME
+    // FILL WORKOUT LIST WITH EXERCISE AND REP SCHEME
     for(i=0; i < exercises.length; i++){
         for(j=0; j < reps.length; j++){
             workoutList[i + j * exercises.length] = {
@@ -39,10 +63,14 @@ function createWorkout(){
         }
     }
     
-    //shuffle(workoutList);
+    // ADD WILDCARD EXERCISES
+    workoutList.push(wildcardExercise1);
+    workoutList.push(wildcardExercise2);
 }
 
+/////////////////////////////////////////////////////////////
 // REMOVE EXERCISE FROM LIST
+/////////////////////////////////////////////////////////////
 function removeExercise(name){
     
     // GET INDEX
@@ -58,7 +86,9 @@ function removeExercise(name){
     
 }
 
+/////////////////////////////////////////////////////////////
 // MOVE EXERCISE FROM WORKOUT LIST TO COMPLETED
+/////////////////////////////////////////////////////////////
 function moveExerciseToComplete(name){
     
     // CHECK IF EXERCISE IS IN WORKOUT LIST
@@ -73,16 +103,31 @@ function moveExerciseToComplete(name){
     }
 }
 
-// UPDATE TOTAL EXERCISE COUNTER
+/////////////////////////////////////////////////////////////
+// UPDATE TOTAL COUNTER ELEMENT/NODE
+/////////////////////////////////////////////////////////////
 function updateCounter(){
     
-    totalCounter = totalCounter + 1;
-    totalCounterEle.innerHTML = totalCounter + "/" + reps.length * exercises.length;
+    // UPDATE BREAK COUNTER
+    settings.breakCounter = settings.breakCounter + 1;
+    
+    // UPDATE EXERCISE COMPLETED COUNTER
+    settings.totalCounter = settings.totalCounter + 1;
+    nodes.totalCounter.innerHTML = settings.totalCounter + "/" + workoutList.length;
+    
+    // CHECK IF WORKOUT IS COMPLETE
+    if(settings.totalCounter >= workoutList.length){
+        workoutComplete = true;
+        
+        // DEACTIVATE BUTTON
+        document.getElementById("next_exercise_btn").disabled = true;
+    }
 
 }
 
-
+/////////////////////////////////////////////////////////////
 // SHUFFLE WORKOUT LIST
+/////////////////////////////////////////////////////////////
 function shuffleWorkoutList(a) {
     var j, x, i;
     for (i = a.length - 1; i > 0; i--) {
@@ -94,51 +139,67 @@ function shuffleWorkoutList(a) {
     return a;
 }
 
-// NEXT EXERCISE
+/////////////////////////////////////////////////////////////
+// UPDATE EXERCISE TO DISPLAY
+// MOVE TO NEXT EXERCISE IN LIST
+/////////////////////////////////////////////////////////////
 function updateExercise(){
     
-    if( breakCounter === 10){
-        exerciseTitleEle.innerHTML = "Take a break";
-        exerciseRepEle.innerHTML = "3 mins";
-        
-        breakCounter = 0;
-    }
+    updateCounter();
     
-    
-    if( totalCounter >= 52 && totalCounter <= 54){
-        exerciseTitleEle.innerHTML = "Do all";
-        exerciseRepEle.innerHTML = "5";
-        
-        
-    }
-    
-    
-    if( totalCounter >= 54 ){
-        exerciseTitleEle.innerHTML = "Way to go!";
-        exerciseRepEle.innerHTML = "Workout is complete.";
-    }
-    else {
-        updateCounter();
+    if( !workoutComplete ){
+        // CHECK IF COUNTER IS AT 10, THEN DISPLAY REST PERIOD
+        if( settings.breakCounter === 10){
 
-        currentExerciseIndex = currentExerciseIndex + 1;
+            settings.breakCounter = 0; // RESET COUNTER TO 0
 
-        displayExercise();
-        breakCounter = breakCounter + 1;
+            // UPDATE TEXT
+            displayExercise("Take a break", "2 mins");
+
+            // START TIMER
+            //startRestTimer();
+        } else {
+            nextExercise();    
+        }
+    } else {
+        nodes.exercise.innerHTML = "Way to go!";
+        nodes.reps.innerHTML = "Workout is complete.";
     }
+    
 }
 
+/////////////////////////////////////////////////////////////
+// MOVE TO NEXT EXERCISE IN LIST
+/////////////////////////////////////////////////////////////
+function nextExercise(){
+    currentExerciseIndex = currentExerciseIndex + 1;
+
+    displayExercise(workoutList[currentExerciseIndex].exercise, workoutList[currentExerciseIndex].reps);
+}
+
+/////////////////////////////////////////////////////////////
+// INITIALIZE APP
+/////////////////////////////////////////////////////////////
 function init(){
     createWorkout();
     shuffleWorkoutList(workoutList);
     
     // DISPLAY
-    displayExercise();
+    updateCounter();
+    displayExercise(workoutList[currentExerciseIndex].exercise, workoutList[currentExerciseIndex].reps);
 }
 
-function displayExercise(){
-    exerciseTitleEle.innerHTML = workoutList[currentExerciseIndex].exercise;
-    exerciseRepEle.innerHTML = workoutList[currentExerciseIndex].reps;
+/////////////////////////////////////////////////////////////
+// UPDATE/DISPLAY EXERCISE AND REPS TEXT
+/////////////////////////////////////////////////////////////
+function displayExercise(main, sub){
+        
+    nodes.exercise.innerHTML = main; // MAIN HEADER TEXT
+    nodes.reps.innerHTML = sub; // SUBHEADER TEXT
+    
 }
 
+/////////////////////////////////////////////////////////////
 // KICKSTART APP
+/////////////////////////////////////////////////////////////
 init();
